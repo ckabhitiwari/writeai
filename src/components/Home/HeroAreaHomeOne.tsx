46b018d6a35bdf3174b647b278026accfee49a90
Abhinav Tiwari <abhinavtiwari@192.168.1.6>
2025-08-13 01:51:23 +0530

    Removed the video button from home page banner

--- src/components/Home/HeroAreaHomeOne.tsx
+++ src/components/Home/HeroAreaHomeOne.tsx
@@ -16,7 +16,6 @@ const HeroAreaHomeOne = () => {
                 <p>Unleash the power of AI to effortlessly create engaging content. Whether you're a marketer, blogger, or business owner, our AI writing tool streamlines your writing process, saves time, and boosts creativity.</p>
                 <div className="uxora-hero-btn mt-50">
                   <a href="contact-us.html" className="uxora-default-btn">Start writing for free</a>
-                  
                 </div>
               </div>
             </div>
